8cf8f8c16d3d7e985ade5d47b5c09d538ddf6e4c
scotts <scotts@esparklearning.com>
2015-09-21 13:25:33 -0500

    initial commit

---
+++ server.R
@@ -0,0 +1,27 @@
+
+# This is the server logic for a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+
+library(shiny)
+
+shinyServer(function(input, output) {
+
+  districts <- reactive({
+    districtproximity::districtsWithinRange(as.numeric(input$lat), as.numeric(input$long), input$radius)
+  })
+  
+  output$districts_table <- renderDataTable({
+    districts()
+  })
+  
+  output$download_districts_table <- downloadHandler(
+    filename = function() {
+      paste0("Disticts_within_",round(input$radius), "_miles_of_lat_", input$lat, "_long_", input$long)},
+    content = function(file) {
+      write.csv(districts(), file)
+    }
+  )
+})
---
+++ ui.R
@@ -0,0 +1,38 @@
+
+# This is the user-interface definition of a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+
+library(shiny)
+library(schooldistricts)
+library(districtproximity)
+
+shinyUI(fluidPage(
+  
+  titlePanel("Search for Nearby Districts"),
+
+  sidebarLayout(
+    sidebarPanel(
+      a("Geolocate an address", href="http://itouchmap.com/latlong.html", target="_blank"),
+      HTML("<br><br>"),
+      textInput("lat",
+                "Latitude:"),
+      textInput("long",
+                "Longitude:"),
+      sliderInput("radius",
+                  "Radius to search in miles",
+                  min = 1,
+                  max = 100,
+                  value = 40)
+    ),
+    
+
+    # Show a plot of the generated distribution
+    mainPanel(
+      dataTableOutput("districts_table"),
+      downloadButton('download_districts_table','Download Table')
+    )
+  )
+))
